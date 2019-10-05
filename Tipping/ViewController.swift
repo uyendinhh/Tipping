49591552290325935b7921fb8d96508b8cd72df1
Uyen Dinh <udinh3@gatech.edu>
2019-10-04 23:30:36 -0700

    add styles

--- Tipping/ViewController.swift
+++ Tipping/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var billField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    
+    @IBOutlet var tipControl: UISegmentedControl!
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //get the bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        //calculate the tip and total
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update the tip and amount
+        tipLabel.text = String(format: "$%0.2f", tip)
+        totalLabel.text = String(format: "$%0.2f", total)
+    }
 }
 
