6510acc6b32688e467189560120364941732c5c1
Uyen Dinh <udinh3@gatech.edu>
2019-10-04 21:47:48 -0700

    Initial Commit

---
+++ Tipping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipping
+//
+//  Created by Uyen Dinh on 10/4/19.
+//  Copyright © 2019 Uyen Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
